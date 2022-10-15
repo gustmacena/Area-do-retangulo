ca9e8115263d5ee1e0289de7d014bf2c14b63d0f
gustmacena <102830712+gustmacena@users.noreply.github.com>
2022-10-14 21:36:38 -0300

    Area do Retangulo

---
+++ Area do retangulo/area_do_retangulo.js
@@ -0,0 +1,8 @@
+function areaRetangulo(b , h){
+
+    let a = b * h
+
+    return  "A area do Retangulo é: " + a 
+}
+
+console.log(areaRetangulo(5, 3))
---
+++ exe02.js
@@ -0,0 +1,7 @@
+let x = 70
+
+if( x % 2 <= 0){
+    console.log("o numero e Par!")
+} else{
+    console.log("o numero e Impar!")
+}
